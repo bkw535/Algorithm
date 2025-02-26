e7e61fc426560b6f18671a3fee12e1019cf1827d
bkw535 <127226556+bkw535@users.noreply.github.com>
2025-02-26 15:56:34 +0900

    [Silver V] Title: 너의 평점은, Time: 108 ms, Memory: 14400 KB -BaekjoonHub

---
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Objects;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        HashMap<String, Double> grade = new HashMap<>();
+        grade.put("A+", 4.5);
+        grade.put("A0", 4.0);
+        grade.put("B+", 3.5);
+        grade.put("B0", 3.0);
+        grade.put("C+", 2.5);
+        grade.put("C0", 2.0);
+        grade.put("D+", 1.5);
+        grade.put("D0", 1.0);
+        grade.put("F", 0.0);
+
+        double sum = 0;
+        double deptSum = 0;
+
+        for(int i=0; i<20; i++){
+            String line = br.readLine();
+            String[] data = line.split(" ");
+            double point = Double.parseDouble(data[1]);
+            double value = 0;
+            if(!Objects.equals(data[2], "P"))
+                value = grade.get(data[2]);
+            else{
+                continue;
+            }
+            sum += point*value;
+            deptSum += point;
+        }
+
+        System.out.println(sum/deptSum);
+    }
+}
