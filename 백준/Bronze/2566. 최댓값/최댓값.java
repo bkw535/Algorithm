841ab4a84da726c617166ed33fda7ba8fc1bbae5
bkw535 <127226556+bkw535@users.noreply.github.com>
2025-02-27 15:18:08 +0900

    [Bronze III] Title: 최댓값, Time: 124 ms, Memory: 15968 KB -BaekjoonHub

---
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[][] matrix = new int[9][9];
+        int max = Integer.MIN_VALUE;
+        int row = 0, col = 0;
+
+        for (int i = 0; i < 9; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 9; j++) {
+                matrix[i][j] = Integer.parseInt(st.nextToken());
+                if (matrix[i][j] > max) {
+                    max = matrix[i][j];
+                    row = i+1;
+                    col = j+1;
+                }
+            }
+        }
+
+        System.out.println(max);
+        System.out.print(row+" "+col);
+    }
+}
