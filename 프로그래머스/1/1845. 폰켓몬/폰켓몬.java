0781e35ac44ec690f91f9eaaf22632e3b97e6112
bkw535 <127226556+bkw535@users.noreply.github.com>
2025-07-01 09:34:17 +0900

    [level 1] Title: 폰켓몬, Time: 5.58 ms, Memory: 90.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        
+        Set<Integer> set = new HashSet<>();
+        
+        for(int i=0; i<nums.length; i++){
+            set.add(nums[i]);
+        }
+        
+        int n = nums.length/2;
+        
+        answer = Math.min(n, set.size());
+        
+        return answer;
+    }
+}
