ec112fd48ad0f5c7520abf5d9b6a1526a38e3f76
bkw535 <127226556+bkw535@users.noreply.github.com>
2025-07-06 01:51:52 +0900

    [Silver V] Title: 나이순 정렬, Time: 620 ms, Memory: 52340 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -1,24 +1,26 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Comparator;
-
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
         int n = Integer.parseInt(br.readLine());
-        String[] input = new String[n];
 
-        for(int i=0; i<n; i++){
-            input[i] = br.readLine();
+        String[][] arr = new String[n][2];
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = st.nextToken();
+            arr[i][1] = st.nextToken();
         }
 
-        Arrays.sort(input, Comparator.comparingInt(s -> Integer.parseInt(s.split(" ")[0])));
+        Arrays.sort(arr, (a, b) -> Integer.parseInt(a[0]) - Integer.parseInt(b[0]));
 
-        for(int i=0; i<n; i++){
-            System.out.println(input[i]);
+        for(int i = 0; i < n; i++){
+            sb.append(arr[i][0]).append(" ").append(arr[i][1]).append("\n");
         }
+
+        System.out.println(sb.toString());
     }
 }
