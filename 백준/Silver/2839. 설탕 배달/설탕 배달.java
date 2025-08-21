147fc9721e2de8344bcc017f8da3ed211d87b942
bkw535 <127226556+bkw535@users.noreply.github.com>
2025-08-22 03:25:50 +0900

    [Silver IV] Title: 설탕 배달, Time: 100 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,26 +1,29 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        long N = Long.parseLong(br.readLine());
-        long num5 = 0;
-        long num3 = 0;
-
-        while (true) {
-            if (N % 5 == 0) {
-                num5 += N / 5;
-                break;
-            }
-            N-=3;
-            num3++;
-            if(N<0){
-                System.out.println(-1);
-                return;
+        int N = Integer.parseInt(br.readLine());
+        int[] dp = new int[5001];
+        
+        dp[3] = 1;
+        dp[5] = 1;
+        
+        for(int i=6; i<=N; i++) {
+            if (dp[i-3] != 0 && dp[i-5] != 0) {
+                dp[i] = Math.min(dp[i-3], dp[i-5]) + 1;
+            } else if (dp[i-3] != 0) {
+                dp[i] = dp[i-3] + 1;
+            } else if (dp[i-5] != 0) {
+                dp[i] = dp[i-5] + 1;
             }
         }
-        System.out.println(num5 + num3);
+        
+        if(dp[N] == 0) {
+            dp[N] = -1;
+        }
+        
+        System.out.println(dp[N]);
     }
 }
