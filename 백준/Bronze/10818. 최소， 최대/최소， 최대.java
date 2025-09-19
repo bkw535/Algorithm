0c4415bf8cca1c4000c0e92a38c544860ee5c1e4
bkw535 <127226556+bkw535@users.noreply.github.com>
2025-09-19 16:58:04 +0900

    [Bronze III] Title: 최소, 최대, Time: 456 ms, Memory: 88128 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,20 +1,21 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.*;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int a = Integer.parseInt(br.readLine());
-        String[] arr = br.readLine().split(" ");
-        int m=Integer.parseInt(arr[0]);
-        int n=Integer.parseInt(arr[0]);
-        for (int i = 0; i < a; i++) {
-            if(m<Integer.parseInt(arr[i]))
-                m=Integer.parseInt(arr[i]);
-            if (n>Integer.parseInt(arr[i]))
-                n=Integer.parseInt(arr[i]);
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int m = Integer.parseInt(st.nextToken());
+        int max = m;
+        int min = m;
+        for(int i=0; i<N-1; i++) {
+            int l = Integer.parseInt(st.nextToken());
+            if(l>max) max = l;
+            if(l<min) min = l;
         }
-        System.out.printf("%d %d\n",n,m);
+        
+        System.out.println(min + " " + max);
     }
 }
