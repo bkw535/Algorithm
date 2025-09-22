942783913f275a8fb2d40b4f74d2fd7468d45f0e
bkw535 <127226556+bkw535@users.noreply.github.com>
2025-09-22 14:55:14 +0900

    [Bronze III] Title: 최댓값, Time: 100 ms, Memory: 14040 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.java
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -1,22 +1,21 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.*;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] arr = new int[9];
-        int m = arr[0];
-        int n = 0;
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-            if(m<arr[i]) {
-                m = arr[i];
-                n = i+1;
+        
+        int max = 0;
+        int index = -1;
+        for(int i=0; i<9; i++) {
+            int n = Integer.parseInt(br.readLine());
+            if(max<n) {
+                max = n;
+                index = i+1;
             }
         }
-        br.close();
-        System.out.println(m);
-        System.out.println(n);
+        
+        System.out.println(max);
+        System.out.println(index);
     }
 }
