4f68e3d12c3e33a7ae0ade0d937930bfafd00ed2
bkw535 <127226556+bkw535@users.noreply.github.com>
2025-10-15 23:33:32 +0900

    [Silver V] Title: 덩치, Time: 96 ms, Memory: 14164 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.java
+++ 백준/Silver/7568. 덩치/덩치.java
@@ -1,33 +1,28 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        StringTokenizer st;
         StringBuilder sb = new StringBuilder();
-        int[][] arr = new int[n][2];
-
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+        
+        int[][] arr = new int[N][2];
+        for(int i=0; i<N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
             arr[i][0] = Integer.parseInt(st.nextToken());
             arr[i][1] = Integer.parseInt(st.nextToken());
         }
-
-        for (int i = 0; i < n; i++) {
-            int rank = 1;
-            for (int j = 0; j < n; j++) {
-                if(i==j) continue;
-
-                if(arr[i][0]<arr[j][0] && arr[i][1]<arr[j][1]) {
-                    rank++;
-                }
+        
+        for(int i=0; i<N; i++) {
+            int count = 0;
+            for(int j=0; j<N; j++) {
+                if(i == j) continue;
+                if(arr[i][0] < arr[j][0] && arr[i][1] < arr[j][1]) count++;
             }
-            sb.append(rank).append(" ");
+            sb.append(count+1).append(" ");
         }
-        System.out.println(sb);
+        
+        System.out.println(sb.toString().trim());
     }
 }
