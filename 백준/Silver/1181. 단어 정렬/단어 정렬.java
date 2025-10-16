84be458bf4aed2de73dde7652e5acfa75e962523
bkw535 <127226556+bkw535@users.noreply.github.com>
2025-10-17 00:27:29 +0900

    [Silver V] Title: 단어 정렬, Time: 332 ms, Memory: 25964 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -5,24 +5,28 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-
+        
         int n = Integer.parseInt(br.readLine());
         Set<String> set = new HashSet<>();
-        for(int i = 0; i < n; i++){
+        
+        for(int i=0; i<n; i++) {
             set.add(br.readLine());
         }
-
-        String[] arr = new String[set.size()];
-        arr = set.toArray(arr);
-
-        Arrays.sort(arr, Comparator
-                .comparingInt(String::length)
-                .thenComparing(Comparator.naturalOrder()));
-
-        for(int i = 0; i < arr.length; i++){
-            sb.append(arr[i]).append("\n");
+        
+        List<String> arr = new ArrayList<>(set);
+        
+        arr.sort((a,b) -> {
+            if(a.length() == b.length()) {
+                return a.compareTo(b);
+            }
+            return a.length() - b.length();
+        });
+         
+        for(int i=0; i<arr.size(); i++) {
+            sb.append(arr.get(i));
+            if(i<n-1) sb.append("\n");
         }
-
-        System.out.println(sb.toString());
+        
+        System.out.println(sb);
     }
 }
