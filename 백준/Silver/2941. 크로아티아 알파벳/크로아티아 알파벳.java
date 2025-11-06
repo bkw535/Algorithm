450dabec95446a69341131bbe22fb3d22c4e59db
bkw535 <127226556+bkw535@users.noreply.github.com>
2025-11-07 00:55:22 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 100 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -1,32 +1,16 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        String[] cr = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String input = br.readLine();
-        int n = 0;
-
-        for (int i = 0; i < input.length(); i++) {
-            boolean found = false;
-
-            for (String croatian : cr) {
-                if (i + croatian.length() <= input.length() && input.substring(i, i + croatian.length()).equals(croatian)) {
-                    n++;
-                    i += croatian.length() - 1;
-                    found = true;
-                    break;
-                }
-            }
-
-            if (!found) {
-                n++;
-            }
+        String[] croatiaWord = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
+        
+        String word = br.readLine();
+        for(int i = 0; i<croatiaWord.length; i++) {
+            word = word.replace(croatiaWord[i], "#");
         }
-
-        System.out.println(n);
+        
+        System.out.println(word.length());
     }
 }
