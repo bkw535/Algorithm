5cbe07acb094303161fc6ce90667d860eac55136
bkw535 <127226556+bkw535@users.noreply.github.com>
2025-11-20 22:07:35 +0900

    [Silver IV] Title: 큐 2, Time: 1312 ms, Memory: 333656 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.java
+++ 백준/Silver/18258. 큐 2/큐 2.java
@@ -1,50 +1,39 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        int n = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+        Deque<Integer> deque = new ArrayDeque<>();
         
-        Queue<Integer> queue = new LinkedList<>();
-        for(int i=0; i<n; i++) {
+        for(int i=0; i<N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            switch(st.nextToken()) {
-                case "push":
-                    queue.offer(Integer.parseInt(st.nextToken()));
-                    break;
-                case "pop":
-                    if(!queue.isEmpty()) {
-                        sb.append(queue.poll()).append("\n");
-                    } else {
-                        sb.append(-1).append("\n");
-                    }
-                    break;
-                case "size":
-                    sb.append(queue.size()).append("\n");
-                    break;
-                case "empty":
-                    if(queue.isEmpty()) {
-                        sb.append(1).append("\n");
-                    } else {
-                        sb.append(0).append("\n");
-                    }
-                    break;
-                case "front":
-                    if(!queue.isEmpty()) {
-                        sb.append(queue.peek()).append("\n");
-                    } else {
-                        sb.append(-1).append("\n");
-                    }
-                    break;
-                case "back":
-                    if(!queue.isEmpty()) {
-                        sb.append(((LinkedList<Integer>) queue).peekLast()).append("\n");
-                    } else {
-                        sb.append(-1).append("\n");
-                    }
-                    break;
+            String order = st.nextToken();
+            
+            if(order.equals("push")) {
+                int num = Integer.parseInt(st.nextToken());
+                deque.addLast(num);
+            }
+            if(order.equals("pop")) {
+                if(deque.isEmpty()) sb.append(-1).append("\n");
+                else sb.append(deque.pop()).append("\n");
+            }
+            if(order.equals("front")) {
+                if(deque.isEmpty()) sb.append(-1).append("\n");
+                else sb.append(deque.peekFirst()).append("\n");
+            }
+            if(order.equals("back")) {
+                if(deque.isEmpty()) sb.append(-1).append("\n");
+                else sb.append(deque.peekLast()).append("\n");
+            }
+            if(order.equals("size")) {
+                sb.append(deque.size()).append("\n");
+            }
+            if(order.equals("empty")) {
+                if(deque.isEmpty()) sb.append(1).append("\n");
+                else sb.append(0).append("\n");
             }
         }
         
