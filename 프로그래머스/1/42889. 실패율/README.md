4fd20fdce873628a0fd52b864145e81c5a740f3a
bkw535 <127226556+bkw535@users.noreply.github.com>
2025-12-18 00:11:00 +0900

    [level 1] Title: 실패율, Time: 144.88 ms, Memory: 84.3 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/README.md
+++ 프로그래머스/1/42889. 실패율/README.md
@@ -1,10 +1,10 @@
 # [level 1] 실패율 - 42889 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42889) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42889?language=java) 
 
 ### 성능 요약
 
-메모리: 97.7 MB, 시간: 377.26 ms
+메모리: 84.3 MB, 시간: 144.88 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 04일 07:05:32
+2025년 12월 18일 00:10:58
 
 ### 문제 설명
 
--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -1,32 +1,36 @@
 import java.util.*;
+import java.util.stream.*;
 
 class Solution {
     public int[] solution(int N, int[] stages) {
-        double[][] rate = new double[N][2];
+        double[] answer = new double[N];
         
-        for(int i=1; i<N+1; i++){
-            int index = 0;
-            int clear = 0;
-            for(int j=0; j<stages.length; j++){
-                if(stages[j]==i) index++;
-                if(stages[j]>=i) clear++;
+        int index = stages.length;
+        
+        for(int j=1; j<=N; j++) {
+            int count = 0;
+            for(int i : stages) {
+                if(i == j) count++;
             }
-            rate[i-1][0] = i;
-            rate[i - 1][1] = (clear == 0) ? 0 : (double) index / clear;
+            
+            if(index == 0) {
+                answer[j-1] = 0;
+            } else {
+                answer[j-1] = (double) count/index;
+            }
+            
+            index -=count;
         }
         
-        Arrays.sort(rate, (a, b) -> {
-            if (Double.compare(b[1], a[1]) == 0) {
-                return Double.compare(a[0], b[0]);
-            }
-            return Double.compare(b[1], a[1]);
-        });
-        
-        int[] answer = new int[N];
-        for (int i = 0; i < N; i++) {
-            answer[i] = (int) rate[i][0];
-        }
-        
-        return answer;
+        return IntStream.range(0, N)
+                .boxed()
+                .sorted((a, b) -> {
+                    if (Double.compare(answer[b], answer[a]) != 0) {
+                        return Double.compare(answer[b], answer[a]);
+                    }
+                    return Integer.compare(a, b);
+                })
+                .mapToInt(i -> i + 1)
+                .toArray();
     }
 }
