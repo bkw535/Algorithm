84c9ffb7cc111e2c1f24b1a9f873c52b291648f9
bkw535 <127226556+bkw535@users.noreply.github.com>
2025-12-20 23:05:56 +0900

    [level 0] Title: 배열 뒤집기, Time: 3.58 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -1,14 +1,11 @@
 import java.util.*;
+import java.util.stream.*;
 
 class Solution {
     public int[] solution(int[] num_list) {
-        List<Integer> list = new ArrayList<>();
-        for(int num : num_list) {
-            list.add(num);
-        }
-        
-        Collections.reverse(list);
-        int[] answer = list.stream().mapToInt(Integer::intValue).toArray();
+        int[] answer = IntStream.range(0, num_list.length)
+            .map(i -> num_list[num_list.length - 1 - i])
+            .toArray();
         
         return answer;
     }
