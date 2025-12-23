91a0cb600fd3b53917c3164ad05971b9573e94d1
bkw535 <127226556+bkw535@users.noreply.github.com>
2025-12-23 16:11:03 +0900

    [level 2] Title: 주식가격, Time: 19.93 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,20 +1,17 @@
 class Solution {
     public int[] solution(int[] prices) {
-        int[] answer = new int[prices.length];
-
-        int index = prices.length;
-
-        for(int i = 0; i < prices.length - 1; i++){
-            for(int j = i+1; j < index; j++){
-                if(prices[i] > prices[j]){
-                    answer[i]++;
+        int[] time = new int[prices.length];
+        
+        for(int i=0; i<prices.length; i++) {
+            int cur = prices[i];
+            for(int j=i+1; j<prices.length; j++) {
+                time[i]++;
+                if(prices[j] < cur) {
                     break;
-                } else {
-                    answer[i]++;
                 }
             }
         }
         
-        return answer;
+        return time;
     }
 }
