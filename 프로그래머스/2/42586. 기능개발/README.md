91b0273a31531a668e06d1c43e7c0e42cd1b9628
bkw535 <127226556+bkw535@users.noreply.github.com>
2025-12-29 20:26:27 +0900

    [level 2] Title: 기능개발, Time: 2.71 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=java) 
 
 ### 성능 요약
 
-메모리: 76.1 MB, 시간: 3.51 ms
+메모리: 76.1 MB, 시간: 2.71 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 24일 00:28:54
+2025년 12월 29일 20:26:25
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,26 +2,35 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        Queue<Integer> queue = new LinkedList<>();
-
-        for (int i = 0; i < progresses.length; i++) {
-            int days = (int) Math.ceil((100.0 - progresses[i]) / speeds[i]);
-            queue.offer(days);
-        }
-
-        List<Integer> result = new ArrayList<>();
-        while (!queue.isEmpty()) {
-            int current = queue.poll();
-            int count = 1;
-
-            while (!queue.isEmpty() && queue.peek() <= current) {
-                queue.poll();
-                count++;
+        int[] arr = new int[speeds.length];
+        
+        for(int i=0; i<speeds.length; i++) {
+            int remain = 100 - progresses[i];
+            
+            if(remain % speeds[i] != 0) {
+                arr[i] = remain / speeds[i] + 1;
+            } else {
+                arr[i] = remain / speeds[i];
             }
-
-            result.add(count);
         }
-
-        return result.stream().mapToInt(Integer::intValue).toArray();
+        
+        List<Integer> result = new ArrayList<>();
+        int days = 0;
+        days = arr[0];
+        int count = 1;
+        for(int i=1; i<arr.length; i++) {
+            if (arr[i] <= days) {
+                count++;
+            } else {
+                result.add(count);
+                count = 1;
+                days = arr[i];
+            }
+        }
+        result.add(count);
+        
+        return result.stream()
+            .mapToInt(Integer::intValue)
+            .toArray();
     }
 }
