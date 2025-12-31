dff93b76139bacd2e9c265eff6db8f7b88a6b767
bkw535 <127226556+bkw535@users.noreply.github.com>
2025-12-31 12:42:21 +0900

    [level 2] Title: 의상, Time: 0.10 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -2,20 +2,17 @@ import java.util.*;
 
 class Solution {
     public int solution(String[][] clothes) {
-        int answer = 1;
-
-        HashMap<String, Integer> map = new HashMap<>();
-
-        for(int i=0; i<clothes.length; i++){
-            map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0) + 1);
-        }
-
-        for(String s: map.keySet()){
-            answer *= map.get(s)+1;
+        Map<String, Integer> map = new HashMap<>();
+        
+        for(int i=0; i<clothes.length; i++) {
+            map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0) +1);
         }
         
-        answer -= 1;
-
-        return answer;
+        int count = 1;
+        for(int i : map.values()) {
+            count *= (i+1);
+        }
+        
+        return count - 1;
     }
 }
