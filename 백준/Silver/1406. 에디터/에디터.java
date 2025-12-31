a1a23ae4a1101afa89e7fdf08d55e8610dba061f
bkw535 <127226556+bkw535@users.noreply.github.com>
2025-12-31 15:38:46 +0900

    [Silver II] Title: 에디터, Time: 592 ms, Memory: 129816 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.java
+++ 백준/Silver/1406. 에디터/에디터.java
@@ -4,40 +4,50 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        
         String s = br.readLine();
         int M = Integer.parseInt(br.readLine());
-        Stack<Character> stackLeft = new Stack<>();
-        Stack<Character> stackRight = new Stack<>();
         
-        char[] arr = s.toCharArray();
+        Stack<String> stackLeft = new Stack<>();
+        Stack<String> stackRight = new Stack<>();
+        
         for(int i=0; i<s.length(); i++) {
-            stackLeft.push(arr[i]);
+            stackLeft.push(s.charAt(i) + "");
         }
         
         for(int i=0; i<M; i++) {
-            String o = br.readLine();
-            String[] order = o.split(" ");
-            if(order[0].equals("L")) {
-                if(!stackLeft.isEmpty()) stackRight.push(stackLeft.pop());
-            } else if(order[0].equals("D")) {
-                if(!stackRight.isEmpty()) stackLeft.push(stackRight.pop());
-            } else if(order[0].equals("B")) {
-                if(!stackLeft.isEmpty()) stackLeft.pop();
-            } else if(order[0].equals("P")){
-                stackLeft.push(order[1].charAt(0));
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String a = st.nextToken();
+            
+            if(a.equals("L")) {
+                if(!stackLeft.isEmpty()) {
+                    stackRight.push(stackLeft.pop());   
+                }
+            }
+            if(a.equals("D")) {
+                if(!stackRight.isEmpty()) {
+                    stackLeft.push(stackRight.pop());   
+                }
+            }
+            if(a.equals("B")) {
+                if(!stackLeft.isEmpty()) {
+                    stackLeft.pop();
+                }
+            }
+            if(a.equals("P")) {
+                String b = st.nextToken();
+                stackLeft.push(b);
             }
         }
         
         while(!stackLeft.isEmpty()) {
             stackRight.push(stackLeft.pop());
         }
-
-        while(!stackRight.isEmpty()) {
+        
+        StringBuilder sb = new StringBuilder();
+        while (!stackRight.isEmpty()) {
             sb.append(stackRight.pop());
         }
-
-        System.out.println(sb);
+        
+        System.out.println(sb.toString());
     }
 }
