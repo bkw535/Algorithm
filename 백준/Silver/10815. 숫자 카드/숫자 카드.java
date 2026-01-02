5b41cea0833705a72c040bb2ebe835885362cba4
bkw535 <127226556+bkw535@users.noreply.github.com>
2026-01-02 12:49:23 +0900

    [Silver V] Title: 숫자 카드, Time: 844 ms, Memory: 136532 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -1,33 +1,28 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int n = Integer.parseInt(br.readLine());
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
         Set<Integer> set = new HashSet<>();
-        for(int i = 0; i<n; i++) {
+        int N = Integer.parseInt(br.readLine());
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++) {
             set.add(Integer.parseInt(st.nextToken()));
         }
-
-        int m = Integer.parseInt(br.readLine());
+        
+        int M = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
-        for(int i = 0; i<m; i++){
-            int aa =  Integer.parseInt(st.nextToken());
-            if(set.contains(aa)){
-                sb.append(1+" ");
-            }else{
-                sb.append(0+" ");
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<M; i++) {
+            if(set.contains(Integer.parseInt(st.nextToken()))) {
+                sb.append(1).append(" ");
+            } else {
+                sb.append(0).append(" ");
             }
         }
-
+        
         System.out.println(sb.toString());
     }
 }
