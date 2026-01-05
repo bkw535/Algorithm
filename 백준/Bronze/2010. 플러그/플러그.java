97f8f71154a461544e2ff890bff70e2ba1329aa5
bkw535 <127226556+bkw535@users.noreply.github.com>
2026-01-05 13:42:05 +0900

    [Bronze III] Title: 플러그, Time: 232 ms, Memory: 41344 KB -BaekjoonHub

---
+++ 백준/Bronze/2010. 플러그/플러그.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        
+        int total = 0;
+        
+        for(int i=0; i<N; i++) {
+            total += (Integer.parseInt(br.readLine()) - 1);    
+        }
+        
+        total += 1;
+        
+        System.out.println(total);
+    }
+}
