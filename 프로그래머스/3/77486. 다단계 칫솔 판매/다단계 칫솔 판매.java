1a955d5b95aa5d4d43ba34f711828be86bc98f6d
bkw535 <127226556+bkw535@users.noreply.github.com>
2026-01-06 11:15:07 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 69.57 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
@@ -3,40 +3,37 @@ import java.util.*;
 class Solution {
     public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
         Map<String, String> parent = new HashMap<>();
-        Map<String, Integer> profit = new HashMap<>();
-        
+        Map<String, Integer> money = new HashMap<>();
         
         for(int i=0; i<enroll.length; i++) {
-            if(referral[i].equals("-")) {
-                parent.put(enroll[i], "center");
-            } else {
-                parent.put(enroll[i], referral[i]);    
+            String a = enroll[i];
+            String b = referral[i];
+            if(b.equals("-")) {
+                b = "center";
             }
-            profit.put(enroll[i], 0);
+            parent.put(a, b);
+            money.put(a,0);
         }
-        profit.put("center", 0);
         
         for(int i=0; i<seller.length; i++) {
-            String sell = seller[i];
-            int money = amount[i] * 100;
+            String cur = seller[i];
+            int curMoney = amount[i] * 100;
             
-            while(!sell.equals("center") && money > 0) {
-                int give = money / 10;
-                int keep = money - give;
+            while(!cur.equals("center") && curMoney > 0) {
+                int up = curMoney/10;
+                int remain = curMoney - up;
                 
-                profit.put(sell, profit.get(sell) + keep);
-
-                sell = parent.get(sell);
-                money = give;
+                money.put(cur, money.get(cur) + remain);
+                cur = parent.get(cur);
+                curMoney = up;
             }
-            profit.put("center", profit.get("center") + money);
         }
         
-        int[] answer = new int[enroll.length];
-        for (int i = 0; i < enroll.length; i++) {
-            answer[i] = profit.get(enroll[i]);
+        int[] arr = new int[enroll.length];
+        for(int i=0; i<enroll.length; i++) {
+            arr[i] = money.get(enroll[i]);
         }
-
-        return answer;
+        
+        return arr;
     }
 }
