18e07b6269efc88fd1d8ba0721456b2b7a2faeef
bkw535 <127226556+bkw535@users.noreply.github.com>
2026-01-06 10:50:24 +0900

    [level 2] Title: 예상 대진표, Time: 0.03 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -2,13 +2,12 @@ class Solution
 {
     public int solution(int n, int a, int b)
     {
-        int round = 0;
-        while(a != b) {
-            a = (a+1)/2;
-            b = (b+1)/2;
-            round++;
+        int answer = 0;
+        for(answer = 0; a!=b ; answer++) {
+            a = (a+1) / 2;
+            b = (b+1) / 2;
         }
         
-        return round;
+        return answer;
     }
 }
