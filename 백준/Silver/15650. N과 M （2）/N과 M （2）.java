8f42a0b3d673bd37b9885278452576e968f2473e
bkw535 <127226556+bkw535@users.noreply.github.com>
2026-01-06 12:22:03 +0900

    [Silver III] Title: N과 M (2), Time: 104 ms, Memory: 14140 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -4,49 +4,33 @@ import java.util.*;
 public class Main {
     static int N, M;
     static int[] arr;
-    static boolean[] visited;
     static StringBuilder sb = new StringBuilder();
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         
         arr = new int[M];
-        visited = new boolean[N+1];
         
-        dfs(0);
-        System.out.println(sb);
+        dfs(0, 1);
+        
+        System.out.println(sb.toString());
     }
     
-    static void dfs(int depth) {
+    static void dfs(int depth, int start) {
         if(depth == M) {
-            if(order(arr)) {
-                for(int i=0; i<M; i++) {
-                    sb.append(arr[i]).append(" ");
-                }
-                sb.append("\n");
+            for(int i=0; i<arr.length; i++) {
+                sb.append(arr[i]).append(" ");
             }
+            sb.append("\n");
             return;
         }
         
-        for(int i=1; i<=N; i++) {
-            if(!visited[i]) {
-                visited[i] = true;
-                arr[depth] = i;
-                
-                dfs(depth + 1);
-                visited[i] = false;
-            }
+        for(int i=start; i<=N; i++) {
+            arr[depth] = i;
+            dfs(depth + 1, i + 1);
         }
-    }
-    
-    static boolean order(int[] arr) {
-        for (int i = 1; i < M; i++) {
-            if (arr[i - 1] > arr[i]) return false;
-        }
-        return true;
     }
 }
