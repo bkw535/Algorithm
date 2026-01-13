2a65c0aec98a3f321334d312d11579f1d7430d6b
bkw535 <127226556+bkw535@users.noreply.github.com>
2026-01-13 12:32:36 +0900

    [Silver I] Title: 미로 탐색, Time: 112 ms, Memory: 14432 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,58 +1,59 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    static ArrayList<ArrayList<Integer>> board;
-    static boolean[][] visited; 
+    static int N, M;
+    static int[][] maze;
+    static boolean[][] visited;
+    static int[] dx = {0, 0, 1, -1};
+    static int[] dy = {1, -1, 0, 0};
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
         
+        maze = new int[N][M];
         visited = new boolean[N][M];
-        
-        board = new ArrayList<>();
         for(int i=0; i<N; i++) {
-            board.add(new ArrayList<>());
-        }
-        
-        for(int i=0; i<N; i++) {
-            String str = br.readLine();
+            String s = br.readLine();
             for(int j=0; j<M; j++) {
-                board.get(i).add(str.charAt(j) - '0');
+                maze[i][j] = s.charAt(j) - '0';
             }
         }
         
-        Deque<int[]> queue = new ArrayDeque<>();
-        queue.offer(new int[]{0,0});
-        visited[0][0] = true; // 방문 여부
-        int[][] distance = new int[N][M]; // 거리 계산
-        distance[0][0] = 1;
+        bfs(0, 0);
         
-        // 방향(상하좌우)
-        int[] dx = {-1, 1, 0, 0};
-        int[] dy = {0, 0, -1, 1};
+        System.out.println(maze[N-1][M-1]);
+    }
+    
+    static void bfs(int x, int y) {
+        Queue<int[]> queue = new ArrayDeque<>();
+        queue.add(new int[]{x, y});
+        visited[x][y] = true;
         
         while(!queue.isEmpty()) {
-            int[] current = queue.poll(); // 현재 위치
-            int x = current[0];
-            int y = current[1];
+            int[] cur = queue.poll();
+            int cx = cur[0];
+            int cy = cur[1];
             
             for(int i=0; i<4; i++) {
-                int nx = x+dx[i];
-                int ny = y+dy[i];
+                int nx = cx + dx[i];
+                int ny = cy + dy[i];
                 
-                if(nx>=0 && nx<N && ny>=0 && ny<M
-                  && board.get(nx).get(ny) == 1 && !visited[nx][ny]) { // 방문 x
-                    queue.offer(new int[]{nx, ny});
-                    visited[nx][ny] = true;
-                    distance[nx][ny] = distance[x][y] + 1; // 거리 갱신
+                if(nx < 0 || ny < 0 || nx >= N || ny >= M) {
+                    continue;
                 }
+                
+                if (maze[nx][ny] == 0) continue;
+                
+                if (visited[nx][ny]) continue;
+
+                visited[nx][ny] = true;
+                maze[nx][ny] = maze[cx][cy] + 1;
+                queue.add(new int[]{nx, ny});
             }
         }
-        
-        System.out.println(distance[N-1][M-1]);
     }
 }
