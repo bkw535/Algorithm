6dc5a6696b3dd827f066a2f4f81bec785014848e
bkw535 <127226556+bkw535@users.noreply.github.com>
2026-04-04 12:17:36 +0900

    [level 2] Title: 기능개발, Time: 2.39 ms, Memory: 80.6 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,28 +2,28 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        ArrayList<Integer> answer = new ArrayList<>();
-        int[] count = new int[speeds.length];
-        
-        for(int i=0; i<speeds.length; i++) {
-            count[i] = (100 - progresses[i]) / speeds[i];
-            if((100 - progresses[i]) % speeds[i] != 0) count[i] += 1;
+        int[] complete = new int[progresses.length];
+        for(int i=0; i<progresses.length; i++) {
+            int c = (100 - progresses[i])/speeds[i];
+            if((100 - progresses[i])%speeds[i] != 0) c++;
+            complete[i] = c;
         }
         
-        int cur = count[0];
-        int num = 1;
-        for(int i=1; i<count.length; i++) {
-            if(cur >= count[i]) num++;
+        ArrayList<Integer> list = new ArrayList<>();
+        int cur = complete[0];
+        int count = 1;
+        
+        for(int i=1; i<complete.length; i++) {
+            if(cur >= complete[i]) count++;
             else {
-                answer.add(num);
-                cur = count[i];
-                num = 1;
+                list.add(count);
+                cur = complete[i];
+                count = 1;
             }
         }
-        answer.add(num);
         
-        return answer.stream()
-                     .mapToInt(Integer::intValue)
-                     .toArray();
+        list.add(count);
+        
+        return list.stream().mapToInt(i -> i).toArray();
     }
 }
