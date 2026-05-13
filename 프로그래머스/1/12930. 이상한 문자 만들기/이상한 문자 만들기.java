1bc053b41a2173af87387f74c6de8d95f9a912d7
bkw535 <127226556+bkw535@users.noreply.github.com>
2026-05-13 15:30:03 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.19 ms, Memory: 63.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String s) {
+        StringBuilder sb = new StringBuilder();
+        
+        int index = 0;
+        for(int i=0; i<s.length(); i++) {
+            char c = s.charAt(i);
+            
+            if(c == ' ') {
+                index = 0;
+                sb.append(c);
+                continue;
+            }
+            
+            if(index %2 == 0) {
+                sb.append(Character.toUpperCase(c));
+            } else {
+                sb.append(Character.toLowerCase(c));
+            }
+            index++;
+        }
+        
+        return sb.toString();
+    }
+}
