d9210d9b176b98969e1967e7863d0e0739db59bc
bkw535 <127226556+bkw535@users.noreply.github.com>
2026-05-13 13:30:14 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 46.52 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -3,21 +3,19 @@ import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
         Map<String, Integer> map = new HashMap<>();
-        
-        for(String s : participant) {
-            map.put(s, map.getOrDefault(s, 0) + 1);
-        }
-        
-        for(String s : completion) {
-            map.put(s, map.get(s) - 1);
-        }
-        
         String answer = "";
-        for(String s : map.keySet()) {
-            if(map.get(s) != 0) {
-                answer = s;
-                break;
-            }
+        
+        for(int i=0; i<participant.length; i++) {
+            map.put(participant[i], map.getOrDefault(participant[i], 0) + 1);
+        }
+        
+        for(int i=0; i<completion.length; i++) {
+            map.put(completion[i], map.get(completion[i]) - 1);
+        }
+        
+        
+        for(String key : map.keySet()) {
+            if(map.get(key) != 0) answer = key;
         }
         
         return answer;
