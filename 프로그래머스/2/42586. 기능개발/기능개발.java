999008f983c8935d4dda87dfc7426395bc78e40b
bkw535 <127226556+bkw535@users.noreply.github.com>
2026-05-18 13:54:20 +0900

    [level 2] Title: 기능개발, Time: 2.00 ms, Memory: 59.8 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,28 +2,34 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        int[] complete = new int[progresses.length];
+        Deque<Integer> deque = new ArrayDeque<>();
+        
+        int date = 0;
+        
         for(int i=0; i<progresses.length; i++) {
-            int c = (100 - progresses[i])/speeds[i];
-            if((100 - progresses[i])%speeds[i] != 0) c++;
-            complete[i] = c;
+            date = 0;
+            date = (100-progresses[i]) / speeds[i];
+            if((100-progresses[i]) % speeds[i] != 0) date++;
+            
+            deque.offer(date);
         }
         
         ArrayList<Integer> list = new ArrayList<>();
-        int cur = complete[0];
-        int count = 1;
         
-        for(int i=1; i<complete.length; i++) {
-            if(cur >= complete[i]) count++;
-            else {
-                list.add(count);
-                cur = complete[i];
-                count = 1;
+        while(!deque.isEmpty()) {
+            int cur = deque.poll();
+            int idx = 1;
+            
+            while(!deque.isEmpty() && deque.peek() <= cur) {
+                deque.poll();
+                idx++;
             }
+            
+            list.add(idx);
         }
         
-        list.add(count);
+        int[] answer = list.stream().mapToInt(Integer::intValue).toArray();
         
-        return list.stream().mapToInt(i -> i).toArray();
+        return answer;
     }
 }
